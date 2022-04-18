f43d15a219a337524370c2522aed212b290c9cd4
AndyTrendygth <andreaskrenn@outlook.com>
2022-04-18 14:30:00 +0200

    terms, privacy, cookies

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,6 +3,7 @@ import Footer from './Footer'
 import Nav from './Nav'
 import { request } from 'graphql-request'
 import useSWR from 'swr'
+import CookieConsent from 'react-cookie-consent'
 
 const fetcher = query => request(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT,query);
 
@@ -24,6 +25,12 @@ const Layout = ({children}) => {
     <Nav categories={data} />
     <main>{children}</main>
     <Footer/>
+    <CookieConsent
+    location='bottom'
+    buttonText='Accept'
+    style={{background: "#374151"}}
+    buttonStyle={{background: "#6d28d9"}}
+    >By using our pages you agree to our <a className='underline hover:bg-purple-700' href="/terms">Terms of Services</a> and our <a className='underline hover:bg-purple-700' href="/privacy">Privacy Policy</a></CookieConsent>
     </>
   )
 }
